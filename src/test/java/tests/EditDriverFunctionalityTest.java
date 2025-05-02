e8f561f646fad7cc21b7710d4311108e7add7a07
nelsontrov <nelsons21@gmail.com>
2025-05-01 19:20:00 -0500

    Fixed code based on comments

--- src/test/java/tests/EditDriverFunctionalityTest.java
+++ src/test/java/tests/EditDriverFunctionalityTest.java
@@ -95,5 +95,97 @@ public class EditDriverFunctionalityTest extends TestBase {
         Assert.assertEquals(expectedName,actualNameInEditPage);
 
     }
+    @Test
+    public void EditDriverWithInvalidDataTest() throws InterruptedException {
+        ElarAppDriversPage elarAppDriversPage=new ElarAppDriversPage();
+        ElarAppUtility elarAppUtility=new ElarAppUtility();
+        elarAppUtility.addNewDriver();
+
+        JavascriptExecutor jse = ((JavascriptExecutor) driver);
+        ElarAppNewDriverPage elarAppNewDriverPage = new ElarAppNewDriverPage();
+        elarAppNewDriverPage.stuffBtn.click();
+        elarAppNewDriverPage.addName.sendKeys("John Doe");
+        elarAppNewDriverPage.localBtn.click();
+        elarAppNewDriverPage.localStateDropBox.click();
+        Thread.sleep(2000);
+        jse.executeScript("window.scrollBy(0, 1000)");
+        elarAppNewDriverPage.newYorkState.click();
+        elarAppNewDriverPage.addPhoneBtn1.click();
+        elarAppNewDriverPage.inputPhone1.sendKeys("2345678900");
+        jse.executeScript("window.scrollBy(0, 200)");
+        jse.executeScript("window.scrollBy(0, 200)");
+        elarAppNewDriverPage.drivingLcnInput.click();
+        elarAppNewDriverPage.drivingLcnInput.sendKeys("06012025");
+        elarAppNewDriverPage.medicalLcnInput.click();
+        elarAppNewDriverPage.medicalLcnInput.sendKeys("06012025");
+        jse.executeScript("window.scrollBy(0, 500)");
+        elarAppNewDriverPage.createBtn.click();
+        Thread.sleep(2000);
+
+        elarAppNewDriverPage.goToEditButton.click();
+        Thread.sleep(5000);
+        jse.executeScript("window.scrollBy(0, 1000)");
+//        String driverId=elarAppNewDriverPage.driverId.getAttribute("value");
+        elarAppNewDriverPage.editButton.click();
+        Thread.sleep(4000);
+        jse.executeScript("window.scrollBy(0, -1800)");
+        Thread.sleep(2000);
+        elarAppNewDriverPage.addName.click();
+        Thread.sleep(2000);
+       jse.executeScript("arguments[0].value = '';",elarAppNewDriverPage.addName);
+
+        Thread.sleep(2000);
+        elarAppNewDriverPage.addName.click();
+        elarAppNewDriverPage.addName.sendKeys("@#$%%^&*");
+        elarAppNewDriverPage.drivingLcnInput.click();
+        Thread.sleep(2000);
+
+        jse.executeScript("window.scrollBy(0, 300)");
+        Thread.sleep(2000);
+//
+//        Thread.sleep(2000);
+//        jse.executeScript("arguments[0].value = '';",elarAppNewDriverPage.drivingLcnInput);
+//        Thread.sleep(2000);
+////        elarAppNewDriverPage.drivingLcnInput.click();
+//        elarAppNewDriverPage.drivingLicenseBtn.click();
+//        Thread.sleep(2000);
+//        jse.executeScript("arguments[0].value = arguments[1];",elarAppNewDriverPage.drivingLcnInput,"07/01/2025");
+////        elarAppNewDriverPage.drivingLcnInput.sendKeys("07012025");
+//        Thread.sleep(2000);
+//        elarAppNewDriverPage.ipassNum.click();
+//        Thread.sleep(2000);
+
+        jse.executeScript("window.scrollBy(0, 5000)");
+        elarAppNewDriverPage.updateButton.click();
+        Thread.sleep(5000);
+        jse.executeScript("window.scrollBy(0, -5000)");
+        String alert = elarAppNewDriverPage.messageAlert.getText();
+        Thread.sleep(5000);
+
+        // Validation steps - Assertion
+        String expectedMessage="Input must contain only alphanumeric and specific punctuation characters";
+       // String expectedDLExp="07/01/2025";
+
+        String actualDLExp=elarAppNewDriverPage.drivingLcnInput.getAttribute("value");
+
+        Assert.assertEquals(expectedMessage,alert);
+//        Assert.assertEquals(expectedDLExp,actualDLExp);
+
+//        Thread.sleep(2000);
+//        jse.executeScript("window.scrollBy(0, -1400)");
+//        elarAppNewDriverPage.backToListButton.click();
+//
+//        elarAppNewDriverPage.allDriversTabButton.click();
+//        Thread.sleep(2000);
+//        elarAppDriversPage.searchBox.click();
+//        elarAppDriversPage.searchBox.sendKeys(driverId+ Keys.ENTER);
+//        Thread.sleep(2000);
+//        List<WebElement> allDriversIds=elarAppNewDriverPage.allDriversFromList;
+//
+//        allDriversIds.get(allDriversIds.size()-1).click();
+//        String actualNameInEditPage=elarAppNewDriverPage.addName.getAttribute("value");
+//        Assert.assertEquals(expectedName,actualNameInEditPage);
+
+    }
 
 }
