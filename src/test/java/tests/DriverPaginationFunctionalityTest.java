2a84400116e3dc4ec4e8db5a71f851695008c9b9
nelsontrov <nelsons21@gmail.com>
2025-05-17 13:40:45 -0500

    Fixed code based on comments

--- src/test/java/tests/DriverPaginationFunctionalityTest.java
+++ src/test/java/tests/DriverPaginationFunctionalityTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.ElarAppDriversPage;
 import pages.ElarAppLoginPage;
@@ -22,9 +23,17 @@ import java.time.Duration;
 
 public class DriverPaginationFunctionalityTest extends TestBase {
 
-    @Test
-    public void driverPaginationFunctionality() throws InterruptedException {
-        JavascriptExecutor jse = ((JavascriptExecutor) driver);
+    @DataProvider(name = "paginationTest")
+    public Object[][] paginationTestData(){
+        return new Object[][]{
+                {5},
+                {10}
+        };
+    }
+
+    @Test(dataProvider = "paginationTest")
+    public void driverPaginationFunctionality(int numberPerPage) throws InterruptedException {
+
         driver = Driver.getDriver();
         driver.get(ConfigReader.getProperty("ElarAppURL"));
         ElarAppLoginPage elarAppLoginPage = new ElarAppLoginPage();
@@ -41,152 +50,26 @@ public class DriverPaginationFunctionalityTest extends TestBase {
 
         Thread.sleep(6000);
         elarAppDriversPage.rowsPerPage.click();
-        elarAppDriversPage.fiveDriversButton.click();
-        int expectedCount = 5; // Change this based on actual expectation
+        int expectedCount=0;
+        if(numberPerPage==5){
+            elarAppDriversPage.fiveDriversButton.click();
+            expectedCount = 5; // Change this based on actual expectation
+        }else if(numberPerPage==10){
+            elarAppDriversPage.tenDriversButton.click();
+            expectedCount = 10; // Change this based on actual expectation
+        }
+
         Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount, "Mismatch in number of drivers displayed!");
 
        while(elarAppDriversPage.goToNextPage.getAttribute("tabIndex").equals("0")){
 
            elarAppDriversPage.goToNextPage.click();
            wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-           int expectedCount2 = 5; // Change this based on actual expectation
-           Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount2, "Mismatch in number of drivers displayed!");
+
+           Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount, "Mismatch in number of drivers displayed!");
        }
 
 
 
-    }
-
-    @Test
-    public void driverPaginationFunctionality2() throws InterruptedException {
-        JavascriptExecutor jse = ((JavascriptExecutor) driver);
-        driver = Driver.getDriver();
-        driver.get(ConfigReader.getProperty("ElarAppURL"));
-        ElarAppLoginPage elarAppLoginPage = new ElarAppLoginPage();
-        elarAppLoginPage.userNameInput.sendKeys(ConfigReader.getProperty("ValidUsername"));
-        elarAppLoginPage.passwordInput.sendKeys(ConfigReader.getProperty("ValidPassword"));
-        elarAppLoginPage.loginButton.click();
-        NewLocationPage newLocationPage = new NewLocationPage();
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        wait.until(ExpectedConditions.elementToBeClickable(newLocationPage.driverBtn));
-        newLocationPage.driverBtn.click();
-        ElarAppDriversPage elarAppDriversPage = new ElarAppDriversPage();
-        jse.executeScript("window.scrollBy(0, 15000)");
-        jse.executeScript("window.scrollBy(0, 15000)");
-        Thread.sleep(6000);
-        elarAppDriversPage.rowsPerPage.click();
-        elarAppDriversPage.tenDriversButton.click();
-        int expectedCount = 10; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-        int expectedCount2 = 10; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount2, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-        int expectedCount3 = 10; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount3, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-        int expectedCount4 = 10; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount4, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-        int expectedCount5 = 10; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount5, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-        int expectedCount6 = 10; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount6, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-        int expectedCount7 = 10; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount7, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-
-        }
-
-
-    }
-
-    @Test
-    public void driverPaginationFunctionality3() throws InterruptedException {
-        JavascriptExecutor jse = ((JavascriptExecutor) driver);
-        driver = Driver.getDriver();
-        driver.get(ConfigReader.getProperty("ElarAppURL"));
-        ElarAppLoginPage elarAppLoginPage = new ElarAppLoginPage();
-        elarAppLoginPage.userNameInput.sendKeys(ConfigReader.getProperty("ValidUsername"));
-        elarAppLoginPage.passwordInput.sendKeys(ConfigReader.getProperty("ValidPassword"));
-        elarAppLoginPage.loginButton.click();
-        NewLocationPage newLocationPage = new NewLocationPage();
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        wait.until(ExpectedConditions.elementToBeClickable(newLocationPage.driverBtn));
-        newLocationPage.driverBtn.click();
-        ElarAppDriversPage elarAppDriversPage = new ElarAppDriversPage();
-        jse.executeScript("window.scrollBy(0, 600)");
-        jse.executeScript("window.scrollBy(0, 300)");
-        jse.executeScript("window.scrollBy(0, 300)");
-        Thread.sleep(2000);
-        elarAppDriversPage.rowsPerPage.click();
-        elarAppDriversPage.twentyFiveButton.click();
-        Thread.sleep(2000);
-        int expectedCount = 25;
-        jse.executeScript("arguments[0].scrollBy(0, 1300);", elarAppDriversPage.virtualScroller);
-        Thread.sleep(2000);
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames2));
-        Assert.assertEquals(elarAppDriversPage.resultNames2.size(), expectedCount, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames2.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames2.get(i));
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames2));
-        int expectedCount2 = 25; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames2.size(), expectedCount2, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames2.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames2.get(i));
-
-        }
-        elarAppDriversPage.goToNextPage.click();
-        wait.until(ExpectedConditions.visibilityOfAllElements(elarAppDriversPage.resultNames));
-        int expectedCount3 = 25; // Change this based on actual expectation
-        Assert.assertEquals(elarAppDriversPage.resultNames.size(), expectedCount3, "Mismatch in number of drivers displayed!");
-
-        for (int i = 0; i < elarAppDriversPage.resultNames.size(); i++) {
-            System.out.println(elarAppDriversPage.resultNames.get(i));
-
-        }
     }
 }
