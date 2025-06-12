eace330e8eeb319316bb59761683844b6454ff2f
kasym_7l11zah <iskendernurbekovich@gmail.com>
2025-06-11 19:02:58 -0500

    addNewDriver test partial implementation

--- src/test/java/tests/BookingApiTest.java
+++ src/test/java/tests/BookingApiTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
@@ -9,31 +8,28 @@ import pojo.BookingDates;
 import pojo.BookingPartialUpdateRequest;
 import pojo.BookingRequest;
 
-import java.awt.print.Book;
-
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 
-public class BookingApiTest {
+public class BookingAPITest {
 
     @Test(groups = {"regression"})
-    public void partialUpdateBookingApiTest(){
-
+    public void partialUpdateBookingAPITest(){
         /*
-        1.Create booking
+        1. Create booking
         2. Partial update booking
         3. Get booking and validate partial update is worked
          */
 
-        //1. Create a book api
-        BookingRequest bookingRequest = new BookingRequest();
+        // 1. Create booking
+
+        BookingRequest bookingRequest=new BookingRequest();
         bookingRequest.setFirstname("John");
         bookingRequest.setLastname("Doe");
         bookingRequest.setTotalprice(111);
         bookingRequest.setDepositpaid(true);
         bookingRequest.setAdditionalneeds("Breakfast");
 
-        BookingDates bookingDates = new BookingDates();
+        BookingDates bookingDates=new BookingDates();
         bookingDates.setCheckin("2025-07-01");
         bookingDates.setCheckout("2025-08-01");
 
@@ -44,64 +40,59 @@ public class BookingApiTest {
                 .and().body(bookingRequest)
                 .and().log().all()
                 .when().post("/booking");
-
         response.then().log().all();
         response.then().statusCode(200);
 
-        BookingCreateResponse bookingCreateResponse = response.getBody().as(BookingCreateResponse.class);
-        int bookingId = bookingCreateResponse.getBookingid();
+        BookingCreateResponse bookingCreateResponse=response.body().as(BookingCreateResponse.class); // Deserialize json to Java object
+        int bookingId=bookingCreateResponse.getBookingid();
 
         // 2. Partial Update booking
 
-        BookingPartialUpdateRequest partialUpdateRequest = new BookingPartialUpdateRequest();
+        BookingPartialUpdateRequest partialUpdateRequest=new BookingPartialUpdateRequest();
         partialUpdateRequest.setFirstname("Johnny");
         partialUpdateRequest.setLastname("Doe");
-        partialUpdateRequest.setAdditionalneeds("Bike");
+        partialUpdateRequest.setAdditionalneeds("Bicycle");
 
-        Response patchResponse = given().baseUri("https://restful-booker.herokuapp.com")
+        Response patchResponse=given().baseUri("https://restful-booker.herokuapp.com")
                 .and().header("Content-Type","application/json")
                 .and().header("Authorization","Basic YWRtaW46cGFzc3dvcmQxMjM=")
                 .and().body(partialUpdateRequest)
+                .and().log().all()
                 .when().patch("/booking/"+bookingId);
         patchResponse.then().log().all();
         patchResponse.then().statusCode(200);
 
-        // 3. Get created booking
-
-        Response getResponse = given().baseUri("https://restful-booker.herokuapp.com")
+        // 3. Get booking
+        Response getResponse=given().baseUri("https://restful-booker.herokuapp.com")
                 .and().header("Accept","application/json")
+                .and().log().all()
                 .when().get("/booking/"+bookingId);
         getResponse.then().log().all();
         getResponse.then().statusCode(200);
 
-
-        // Validation steps
-
+        // Validation step
         getResponse.then().body("firstname", Matchers.equalTo("Johnny"))
-                .body("additionalneeds",Matchers.equalTo("Bike"))
+                .body("additionalneeds",Matchers.equalTo("Bicycle"))
                 .body("bookingdates.checkin",Matchers.equalTo("2025-07-01"));
-
-
-
     }
 
-    @Test (groups = {"regression"})
+    @Test(groups = {"regression"})
     public void deleteBookingAPITest(){
         /*
-        1. Create bookig
-        2.delete booking
-        3. get booking and validate 404 status code
+        1. Create booking
+        2. Delete booking
+        3. Get booking and validate 404 status code
          */
 
-        // 1. create booking
-        BookingRequest bookingRequest = new BookingRequest();
+        // 1. Create booking
+        BookingRequest bookingRequest=new BookingRequest();
         bookingRequest.setFirstname("John");
         bookingRequest.setLastname("Doe");
         bookingRequest.setTotalprice(111);
         bookingRequest.setDepositpaid(true);
         bookingRequest.setAdditionalneeds("Breakfast");
 
-        BookingDates bookingDates = new BookingDates();
+        BookingDates bookingDates=new BookingDates();
         bookingDates.setCheckin("2025-07-01");
         bookingDates.setCheckout("2025-08-01");
 
@@ -112,27 +103,47 @@ public class BookingApiTest {
                 .and().body(bookingRequest)
                 .and().log().all()
                 .when().post("/booking");
-
         response.then().log().all();
         response.then().statusCode(200);
 
-        BookingCreateResponse bookingCreateResponse = response.getBody().as(BookingCreateResponse.class);
-        int bookingId = bookingCreateResponse.getBookingid();
+        BookingCreateResponse bookingCreateResponse=response.body().as(BookingCreateResponse.class); // Deserialize json to Java object
+        int bookingId=bookingCreateResponse.getBookingid();
 
-        // 2. delete booking
-
-        Response deleteResponse = given().baseUri("https://restful-booker.herokuapp.com")
+        // 2. Delete booking
+        Response deleteResponse=given().baseUri("https://restful-booker.herokuapp.com")
                 .and().header("Authorization","Basic YWRtaW46cGFzc3dvcmQxMjM=")
                 .and().header("Cookie","token=<f43bd5759f84157>")
+                .and().log().all()
                 .when().delete("/booking/"+bookingId);
         deleteResponse.then().log().all();
         deleteResponse.then().statusCode(201);
 
-        // 3. get booking and validate 404 status code
-        Response getResponse = given().baseUri("https://restful-booker.herokuapp.com")
+        // 3. Get booking and validate 404 status code
+        Response getResponse=given().baseUri("https://restful-booker.herokuapp.com")
                 .and().header("Accept","application/json")
+                .and().log().all()
                 .when().get("/booking/"+bookingId);
         getResponse.then().log().all();
         getResponse.then().statusCode(404);
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
